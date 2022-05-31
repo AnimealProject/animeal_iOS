0414b903f7953ca2f7a71dd713cc4a36634c0ea5
Ihar Tsimafeichyk <Ihar_Tsimafeichyk@epam.com>
2022-05-31 17:22:46 +0200

    EPMEDU-688: Add code generation for assets catalog

--- animeal/src/HomeViewController.swift
+++ animeal/src/HomeViewController.swift
@@ -11,6 +11,21 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .systemBackground
+
+        let imageView = UIImageView(image: Asset.Images.animealLogo.image)
+        view.addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+
+        let title = UILabel()
+        view.addSubview(title)
+        title.translatesAutoresizingMaskIntoConstraints = false
+        title.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        title.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 12).isActive = true
+
+        title.text = "Animeal"
+        title.textColor = Asset.Colors.tiffany.color
     }
 }
