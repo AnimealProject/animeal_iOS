14bf5302d31f8613c2aac58e238a457e2ac1e51c
Yauheni Hubar <yauheni_hubar@epam.com>
2022-06-01 13:15:54 +0300

    Add animealStyle target and UIComponents package

---
+++ UIComponents/Sources/UIComponents/UIComponent.swift
@@ -0,0 +1,6 @@
+public struct UIComponent {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ animealStyle/MainViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+
