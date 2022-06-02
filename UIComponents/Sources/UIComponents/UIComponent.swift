863f39b5d40064a657d66ec23df488bd6ec32c68
Yauheni Hubar <yauheni_hubar@epam.com>
2022-06-02 15:35:08 +0300

    add package Style and make it as dependency for UIComponent

--- UIComponents/Sources/UIComponents/UIComponent.swift
+++ UIComponents/Sources/UIComponents/UIComponent.swift
@@ -1,6 +1,14 @@
+import Style
+
 public struct UIComponent {
     public private(set) var text = "Hello, World!"
 
     public init() {
+    }
+    
+    public func foo() {
+        let a = AnyStyle()
+        
+        print(a.text)
     }
 }
