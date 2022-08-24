783a03e419cb659548a9573cc2d7db000f83db53
Ihar Tsimafeichyk <igarekas@gmail.com>
2022-08-24 13:28:46 +0200

    EPMEDU-596: Add  ParagraphView

---
+++ UIComponents/Sources/UIComponents/Components/Texts/ParagraphView.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+public extension ParagraphView {
+    struct Model {
+        public let title: String
+
+        public init(
+            title: String
+        ) {
+            self.title = title
+        }
+    }
+}
+
+public final class ParagraphView: UIView {
+    // MARK: - Private properties
+    private lazy var titleView: UITextView = {
+        let view = UITextView().prepareForAutoLayout()
+        view.textColor = designEngine.colors.textPrimary.uiColor
+        view.font = designEngine.fonts.primary.light(14).uiFont
+        view.textAlignment = .left
+        view.sizeToFit()
+        view.isScrollEnabled = false
+        return view
+    }()
+
+    // MARK: - Initialization
+    public init() {
+        super.init(frame: CGRect.zero)
+        setup()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Configuration
+    public func configure(_ model: Model) {
+        titleView.text = model.title
+    }
+
+    // MARK: - Setup
+    private func setup() {
+        addSubview(titleView)
+        titleView.leadingAnchor ~= leadingAnchor
+        titleView.trailingAnchor ~= trailingAnchor
+        titleView.topAnchor ~= topAnchor
+        titleView.bottomAnchor ~= bottomAnchor
+    }
+}
