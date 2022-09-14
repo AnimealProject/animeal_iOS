402a738cb63adda931253f3213d29a6ccc03df84
Diana Tynkovan <dianadiana.tyn@gmail.com>
2022-09-14 21:10:35 +0400

    NO-JIRA: Update amplify setting up flow

--- update_amplify.sh
+++ update_amplify.sh
@@ -3,13 +3,15 @@ set -e
 IFS='|'
 
 # Read arguments passed to a script:
-while getopts a:s:i:e: flag
+while getopts a:s:i:e:f:fs flag
 do
     case "${flag}" in
         a) AWS_ACCESS_KEY_ID=${OPTARG};;
         s) AWS_SECRET_ACCESS_KEY=${OPTARG};;
         i) APP_ID=${OPTARG};;
-		e) AMPLIFY_ENV=${OPTARG};;
+        e) AMPLIFY_ENV=${OPTARG};;
+        f) AMPLIFY_FACEBOOK_CLIENT_ID=${OPTARG};;
+        fs) AMPLIFY_FACEBOOK_CLIENT_SECRET=${OPTARG};;
     esac
 done
 
@@ -18,6 +20,8 @@ done
 #echo "AWS_SECRET_ACCESS_KEY: $AWS_SECRET_ACCESS_KEY";
 #echo "APP_ID: $APP_ID";
 #echo "AMPLIFY_ENV: $AMPLIFY_ENV";
+#echo "AMPLIFY_FACEBOOK_CLIENT_ID: $AMPLIFY_FACEBOOK_CLIENT_ID"
+#echo "AMPLIFY_FACEBOOK_CLIENT_SECRET: $AMPLIFY_FACEBOOK_CLIENT_SECRET"
 
 
 AWSCLOUDFORMATIONCONFIG="{\
@@ -40,6 +44,13 @@ FRONTEND="{\
 PROVIDERS="{\
 \"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
 }"
+AUTHCONFIG="{\
+\"facebookAppIdUserPool\":\"${AMPLIFY_FACEBOOK_CLIENT_ID}\",\
+\"facebookAppSecretUserPool\":\"${AMPLIFY_FACEBOOK_CLIENT_SECRET}\",\
+}"
+CATEGORIES="{\
+\"auth\":$AUTHCONFIG\
+}"
 
 # For debug purposes
 # echo $AMPLIFY
@@ -52,6 +63,7 @@ amplify pull \
 --amplify $AMPLIFY \
 --frontend $FRONTEND \
 --providers $PROVIDERS \
+--categories $CATEGORIES \
 --yes
 
 # Sometimes it's required to run codegen explicitly
