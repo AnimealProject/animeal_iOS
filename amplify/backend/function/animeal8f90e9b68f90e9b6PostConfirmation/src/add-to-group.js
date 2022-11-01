a41d5a20fac3c7a9a07be7376076262075795462
Ihar Tsimafeichyk <igarekas@gmail.com>
2022-11-01 10:58:16 +0100

    Added latest updates for amplify

---
+++ amplify/backend/function/animeal8f90e9b68f90e9b6PostConfirmation/src/add-to-group.js
@@ -0,0 +1,52 @@
+const aws = require('aws-sdk');
+
+const cognitoidentityserviceprovider = new aws.CognitoIdentityServiceProvider();
+
+/**
+ * @type {import('@types/aws-lambda').PostConfirmationTriggerHandler}
+ */
+exports.handler = async (event) => {
+  console.log(JSON.stringify(event));
+  const groupParams = {
+    GroupName: process.env.GROUP,
+    UserPoolId: event.userPoolId,
+  };
+  const addUserParams = {
+    GroupName: process.env.GROUP,
+    UserPoolId: event.userPoolId,
+    Username: event.userName,
+  };
+  try {
+    await cognitoidentityserviceprovider.getGroup(groupParams).promise();
+  } catch (e) {
+    await cognitoidentityserviceprovider.createGroup(groupParams).promise();
+  }
+
+  await cognitoidentityserviceprovider
+    .adminAddUserToGroup(addUserParams)
+    .promise();
+  if (event.request.userAttributes.email) {
+    if (
+      event.request.userAttributes['cognito:user_status'] ===
+      'EXTERNAL_PROVIDER'
+    ) {
+      console.log('social');
+
+      var params = {
+        UserAttributes: [
+          {
+            Name: 'email_verified',
+            Value: 'true',
+          },
+        ],
+        UserPoolId: event.userPoolId,
+        Username: event.userName,
+      };
+
+      await cognitoidentityserviceprovider
+        .adminUpdateUserAttributes(params)
+        .promise();
+    }
+  }
+  return event;
+};
--- amplify/backend/function/animeal8f90e9b68f90e9b6PreSignup/src/custom.js
+++ amplify/backend/function/animeal8f90e9b68f90e9b6PreSignup/src/custom.js
@@ -1,10 +1,12 @@
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
- exports.handler = async (event, context) => {
+exports.handler = async (event, context) => {
   console.log('Received EVENT: ', JSON.stringify(event, null, 2));
+  if (event.request.userAttributes.phone_number) {
+    event.response.autoVerifyPhone = true;
+  }
   event.response.autoConfirmUser = true;
-  event.response.autoVerifyPhone = true;
   console.log('Received EVENT: ', JSON.stringify(event, null, 2));
   return event;
 };
