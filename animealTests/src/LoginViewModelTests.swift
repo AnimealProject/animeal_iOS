d6a6ec0067df549a491afc1bf1d4e0e1dccfde3c
Pran Kishore <kishorepran@gmail.com>
2023-09-13 19:08:27 +0530

    EPMEDU-2637-Remove Quick and Nimble dependency (#204)
    To reduce the number of 3rd party libraries in our app the idea is to remove Quick and Nimble from from project as dependency.
    Discussed with Mikhail as of now unit test for this project is not maintained.
    And going further any new tests written will need Quick Nimble will be decided later as per use case and latest tech at that point of time.
    Note:
    As of now we cannot execute unit test from the command line due to the fact we are calling Firebase related API from non main thread, without initialising firebase from main thread. As a part of my improvement effort I will be fixing this issue to ensure all firebase and crashlytics startup from main thread itself before we use it in any form. I expect to raise this pr soon.

--- animealTests/src/LoginViewModelTests.swift
+++ animealTests/src/LoginViewModelTests.swift
@@ -1,10 +1,106 @@
-import Quick
-import Nimble
-import UIKit
+import XCTest
 @testable import animeal
 
 @MainActor
-class LoginViewModelTestsSpec: QuickSpec {
+class LoginViewModelTests: XCTestCase {
+    private var sut: LoginViewModel!
 
-    override class func spec() { }
+    override func setUpWithError() throws {
+        let coordinator = LoginCoordinatorMock()
+        let model = LoginModel(
+            providers: [
+                LoginActionType.signInViaPhoneNumber: nil
+            ]
+        )
+        let viewModel = LoginViewModel(
+            model: model,
+            coordinator: coordinator,
+            actionsMapper: LoginViewActionMapper(),
+            onboardingMapper: LoginViewOnboardingStepsMapper()
+        )
+        sut = viewModel
+    }
+
+    override func tearDownWithError() throws {
+        sut = nil
+    }
+
+    func testPreparedForOnboarding() throws {
+        // GIVEN
+        // Prepare the view model with required data
+        let onboardingStepsHaveBeenPrepared = XCTestExpectation(description: "OnboardingStepsHaveBeenPrepared")
+        let actionsHaveBeenPrepaped = XCTestExpectation(description: "ActionsHaveBeenPrepaped")
+        var loginViewAction: LoginViewAction?
+        var loginViewOnboardingStep: LoginViewOnboardingStep?
+        sut.onOnboardingStepsHaveBeenPrepared = { steps in
+            loginViewOnboardingStep = steps.first
+            onboardingStepsHaveBeenPrepared.fulfill()
+        }
+
+        sut.onActionsHaveBeenPrepaped = { actions in
+            loginViewAction = actions.first
+            actionsHaveBeenPrepaped.fulfill()
+        }
+        // WHEN
+        sut.load()
+
+        // THEN
+        wait(for: [onboardingStepsHaveBeenPrepared, actionsHaveBeenPrepaped], timeout: 2)
+        let actionIdentifier = try XCTUnwrap(loginViewAction?.identifier)
+        let loginViewOnboardingStepIdentifier = try XCTUnwrap(loginViewOnboardingStep?.title)
+        XCTAssertEqual(actionIdentifier, "signInViaPhoneNumber")
+        XCTAssertEqual(loginViewOnboardingStepIdentifier, "Take care of pets")
+    }
+}
+
+// MARK: - Mock Co-ordinator
+class LoginCoordinatorMock: LoginCoordinatable {
+    var navigator: Navigating {
+        get { return underlyingNavigator }
+        set(value) { underlyingNavigator = value }
+    }
+    var underlyingNavigator: Navigating!
+
+    // MARK: - moveFromLogin
+    var moveFromLoginToCallsCount = 0
+    var moveFromLoginToCalled: Bool {
+        return moveFromLoginToCallsCount > 0
+    }
+    var moveFromLoginToReceivedRoute: LoginRoute?
+    var moveFromLoginToReceivedInvocations: [LoginRoute] = []
+    var moveFromLoginToClosure: ((LoginRoute) -> Void)?
+
+    func moveFromLogin(to route: LoginRoute) {
+        moveFromLoginToCallsCount += 1
+        moveFromLoginToReceivedRoute = route
+        moveFromLoginToReceivedInvocations.append(route)
+        moveFromLoginToClosure?(route)
+    }
+
+    // MARK: - start
+    var startCallsCount = 0
+    var startCalled: Bool {
+        return startCallsCount > 0
+    }
+    var startClosure: (() -> Void)?
+
+    @MainActor
+    func start() {
+        startCallsCount += 1
+        startClosure?()
+    }
+
+    // MARK: - stop
+
+    var stopCallsCount = 0
+    var stopCalled: Bool {
+        return stopCallsCount > 0
+    }
+    var stopClosure: (() -> Void)?
+
+    @MainActor
+    func stop() {
+        stopCallsCount += 1
+        stopClosure?()
+    }
 }
