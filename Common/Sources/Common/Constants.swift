dc4fe6d3db6a4a89ae5276d0cc8fdd9455dded7e
SebastianFernandez <davidsebastianfp@gmail.com>
2025-09-17 10:39:00 -0600

    EPMEDU-4821: Apply code review suggestions

--- Common/Sources/Common/Constants.swift
+++ Common/Sources/Common/Constants.swift
@@ -5,8 +5,8 @@
 //  Created by Sebastián Fernández on 9/8/25.
 //
 
-public struct Constants { // `public` makes it available outside the file/module
-    public struct URLs {
+public enum Constants { // `public` makes it available outside the file/module
+    public enum URLs {
         public static let termsAndConditions = "https://animalproject.ge/about.html"
         public static let privacyPolicy = "https://animalproject.ge/contact.html"
     }
