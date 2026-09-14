86a7f626d6140f247f85c78a51ab0c9d5d4936a9
SonaHunanyan <62184420+SonaHunanyan@users.noreply.github.com>
2026-09-15 00:33:30 +0400

    EPMEDU-4959: [Animeal][IOS] Visual Distinction Between Editable and Read-Only Profile Fields (#309)

--- animeal/src/Flows/Profile/Model/Items/ProfileModelAction.swift
+++ animeal/src/Flows/Profile/Model/Items/ProfileModelAction.swift
@@ -214,16 +214,6 @@ final class ProfileModelSaveAction: ProfileModelAction {
     }
 
     func execute() async throws -> ProfileModelIntermediateStep? {
-        let items = await state.items
-        let editableItems = items.toEditable { item in
-            switch item.type {
-            case .name, .surname, .email:
-                return false
-            case .phone, .birthday:
-                return true
-            }
-        }
-        await state.updateItems(editableItems)
         return .proceed
     }
 }
